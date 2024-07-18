dd9c18e8f9143d3db3795ee55b5473826d43bdf0
Rivio98 <rivaldogjoni1998@gmail.com>
2024-07-18 17:50:24 +0200

    [FIX] Fixed newtask const

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
+            newTask: null,
             todo: [
                 {
                     text: 'Fare l\'esercizio di Boolean',
